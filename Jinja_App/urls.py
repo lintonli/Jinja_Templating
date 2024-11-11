236670f8324d6f4a4e515ed8bd874f772b89fc70
linton <lintonli162@gmail.com>
2024-11-11 09:08:22 +0300

    Introduced to Jinja and did practice on it

---
+++ Jinja_App/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from Jinja_App import  views
+urlpatterns= [
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('services/', views.services, name='services'),
+]
